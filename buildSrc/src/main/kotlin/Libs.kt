04be41c4fdb5b99cd265846e80553bbc52e52300
C0ng_yun <s25069@gsm.hs.kr>
2026-04-05 22:11:52 +0900

    docs(global) : .idea 트래킹 제거 및 gitignore 조정

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,18 +1,25 @@
 object Libs {
-    const val THE_SDK = "com.github.themoment-team:the-sdk:1.4"
+    // SDK
+    const val THE_SDK = "com.github.themoment-team:the-sdk:1.5"
 
+    // Spring Boot
     const val SPRING_BOOT_JPA = "org.springframework.boot:spring-boot-starter-data-jpa"
     const val SPRING_BOOT_REDIS = "org.springframework.boot:spring-boot-starter-data-redis"
     const val SPRING_BOOT_GRAPHQL = "org.springframework.boot:spring-boot-starter-graphql"
     const val SPRING_BOOT_SECURITY = "org.springframework.boot:spring-boot-starter-security"
     const val SPRING_BOOT_WEB = "org.springframework.boot:spring-boot-starter-web"
 
+    // Kotlin
     const val JACKSON_KOTLIN = "com.fasterxml.jackson.module:jackson-module-kotlin"
     const val KOTLIN_REFLECT = "org.jetbrains.kotlin:kotlin-reflect"
 
-    const val LOMBOK = "org.projectlombok:lombok"
+    // Database
     const val MYSQL_CONNECTOR = "com.mysql:mysql-connector-j"
 
+    // Util
+    const val LOMBOK = "org.projectlombok:lombok"
+
+    // Test
     const val SPRING_BOOT_TEST = "org.springframework.boot:spring-boot-starter-test"
     const val KOTLIN_TEST_JUNIT5 = "org.jetbrains.kotlin:kotlin-test-junit5"
     const val SPRING_GRAPHQL_TEST = "org.springframework.graphql:spring-graphql-test"
