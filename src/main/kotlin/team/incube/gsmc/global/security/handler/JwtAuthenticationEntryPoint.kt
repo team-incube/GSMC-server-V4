25e6f412976240d608706f81913d04c0981ae9ca
jyx-07 <s25069@gsm.hs.kr>
2026-05-21 17:34:31 +0900

    feat(global): 401 인증 예외 핸들러 추가

---
+++ src/main/kotlin/team/incube/gsmc/global/security/handler/JwtAuthenticationEntryPoint.kt
@@ -0,0 +1,27 @@
+package team.incube.gsmc.global.security.handler
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.MediaType
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.stereotype.Component
+import team.incube.gsmc.global.exception.ErrorCode
+import team.incube.gsmc.global.exception.ErrorResponse
+
+@Component
+class JwtAuthenticationEntryPoint(
+    private val objectMapper: ObjectMapper,
+) : AuthenticationEntryPoint {
+    override fun commence(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        authException: AuthenticationException,
+    ) {
+        response.status = HttpServletResponse.SC_UNAUTHORIZED
+        response.contentType = MediaType.APPLICATION_JSON_VALUE
+        response.characterEncoding = "UTF-8"
+        objectMapper.writeValue(response.writer, ErrorResponse(401, ErrorCode.INVALID_TOKEN.message))
+    }
+}
