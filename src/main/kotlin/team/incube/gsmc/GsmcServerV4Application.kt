1b1d41cb58388d17a16b70ce9f3f30fe25f8e133
jyx-07 <s25069@gsm.hs.kr>
2026-05-21 16:05:53 +0900

    feat(global): JwtProperties 설정 프로퍼티 등록

--- src/main/kotlin/team/incube/gsmc/GsmcServerV4Application.kt
+++ src/main/kotlin/team/incube/gsmc/GsmcServerV4Application.kt
@@ -1,9 +1,12 @@
 package team.incube.gsmc
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import team.incube.gsmc.global.security.jwt.JwtProperties
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties::class)
 class GsmcServerV4Application
 
 fun main(args: Array<String>) {
