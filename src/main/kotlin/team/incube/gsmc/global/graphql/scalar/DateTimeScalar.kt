c0c878bd0b3af53fcbcdfbe8c7bd7308d7daf3c0
jyx-07 <s25069@gsm.hs.kr>
2026-06-30 08:10:14 +0900

    fix(global): DateTimeScalar ktlint 포맷 수정

--- src/main/kotlin/team/incube/gsmc/global/graphql/scalar/DateTimeScalar.kt
+++ src/main/kotlin/team/incube/gsmc/global/graphql/scalar/DateTimeScalar.kt
@@ -16,38 +16,59 @@ import java.time.format.DateTimeFormatter
 import java.util.Locale
 
 object DateTimeScalar {
-    val DateTime: GraphQLScalarType = GraphQLScalarType.newScalar()
-        .name("DateTime")
-        .description("ISO-8601 날짜/시간 스칼라 (예: 2024-01-01T12:00:00+09:00)")
-        .coercing(object : Coercing<OffsetDateTime, String> {
-            override fun serialize(dataFetcherResult: Any, graphQLContext: GraphQLContext, locale: Locale): String =
-                when (dataFetcherResult) {
-                    is OffsetDateTime -> dataFetcherResult.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
-                    is LocalDateTime -> dataFetcherResult.atOffset(ZoneOffset.UTC)
-                        .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
-                    is String -> dataFetcherResult
-                    else -> throw CoercingSerializeException("DateTime으로 직렬화할 수 없는 타입: ${dataFetcherResult::class}")
-                }
+    val DateTime: GraphQLScalarType =
+        GraphQLScalarType
+            .newScalar()
+            .name("DateTime")
+            .description("ISO-8601 날짜/시간 스칼라 (예: 2024-01-01T12:00:00+09:00)")
+            .coercing(
+                object : Coercing<OffsetDateTime, String> {
+                    override fun serialize(
+                        dataFetcherResult: Any,
+                        graphQLContext: GraphQLContext,
+                        locale: Locale,
+                    ): String =
+                        when (dataFetcherResult) {
+                            is OffsetDateTime -> dataFetcherResult.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                            is LocalDateTime ->
+                                dataFetcherResult
+                                    .atOffset(ZoneOffset.UTC)
+                                    .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                            is String -> dataFetcherResult
+                            else -> throw CoercingSerializeException(
+                                "DateTime으로 직렬화할 수 없는 타입: ${dataFetcherResult::class}",
+                            )
+                        }
 
-            override fun parseValue(input: Any, graphQLContext: GraphQLContext, locale: Locale): OffsetDateTime =
-                when (input) {
-                    is String -> runCatching { OffsetDateTime.parse(input, DateTimeFormatter.ISO_OFFSET_DATE_TIME) }
-                        .getOrElse { throw CoercingParseValueException("유효하지 않은 DateTime 값: $input") }
-                    else -> throw CoercingParseValueException("DateTime으로 파싱할 수 없는 타입: ${input::class}")
-                }
+                    override fun parseValue(
+                        input: Any,
+                        graphQLContext: GraphQLContext,
+                        locale: Locale,
+                    ): OffsetDateTime =
+                        when (input) {
+                            is String ->
+                                runCatching { OffsetDateTime.parse(input, DateTimeFormatter.ISO_OFFSET_DATE_TIME) }
+                                    .getOrElse { throw CoercingParseValueException("유효하지 않은 DateTime 값: $input") }
+                            else -> throw CoercingParseValueException("DateTime으로 파싱할 수 없는 타입: ${input::class}")
+                        }
 
-            override fun parseLiteral(
-                input: Value<*>,
-                variables: CoercedVariables,
-                graphQLContext: GraphQLContext,
-                locale: Locale,
-            ): OffsetDateTime =
-                when (input) {
-                    is StringValue -> runCatching {
-                        OffsetDateTime.parse(input.value, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
-                    }.getOrElse { throw CoercingParseLiteralException("유효하지 않은 DateTime 리터럴: ${input.value}") }
-                    else -> throw CoercingParseLiteralException("DateTime으로 파싱할 수 없는 리터럴 타입: ${input::class}")
-                }
-        })
-        .build()
+                    override fun parseLiteral(
+                        input: Value<*>,
+                        variables: CoercedVariables,
+                        graphQLContext: GraphQLContext,
+                        locale: Locale,
+                    ): OffsetDateTime =
+                        when (input) {
+                            is StringValue ->
+                                runCatching {
+                                    OffsetDateTime.parse(input.value, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+                                }.getOrElse {
+                                    throw CoercingParseLiteralException(
+                                        "유효하지 않은 DateTime 리터럴: ${input.value}",
+                                    )
+                                }
+                            else -> throw CoercingParseLiteralException("DateTime으로 파싱할 수 없는 리터럴 타입: ${input::class}")
+                        }
+                },
+            ).build()
 }
