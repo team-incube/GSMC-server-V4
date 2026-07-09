b956c4b6eb13c9ed18a4dd598ab17682e9d0249f
jyx-07 <s25069@gsm.hs.kr>
2026-07-10 00:37:08 +0900

    feat(file): 파일 조회 및 점수 연결 포트 구현

---
+++ src/main/kotlin/team/incube/gsmc/domain/file/port/out/FilePersistencePort.kt
@@ -0,0 +1,43 @@
+package team.incube.gsmc.domain.file.port.out
+
+import team.incube.gsmc.domain.file.File
+
+/**
+ * 업로드 파일 영속성을 추상화하는 아웃바운드 포트 인터페이스입니다.
+ */
+interface FilePersistencePort {
+    /**
+     * ID로 파일을 조회한다.
+     *
+     * @param fileId 조회할 파일 ID
+     * @return 해당 파일, 없으면 null
+     */
+    fun findById(fileId: Long): File?
+
+    /**
+     * 특정 점수 요청에 직접 첨부된 파일을 조회한다.
+     *
+     * @param scoreId 조회할 점수 요청 ID
+     * @return 해당 점수 요청에 첨부된 파일, 없으면 null
+     */
+    fun findByScoreId(scoreId: Long): File?
+
+    /**
+     * 파일을 점수 요청에 직접 연결한다(score FK 갈아끼움). [team.incube.gsmc.domain.category.EvidenceType.FILE]
+     * 증빙 카테고리에서 사용한다.
+     *
+     * @param fileId 연결할 파일 ID
+     * @param scoreId 연결할 점수 요청 ID
+     */
+    fun linkToScore(
+        fileId: Long,
+        scoreId: Long,
+    )
+
+    /**
+     * 파일의 점수 요청 연결을 해제한다(score FK를 null로 갈아끼움).
+     *
+     * @param fileId 연결을 해제할 파일 ID
+     */
+    fun unlinkFromScore(fileId: Long)
+}
