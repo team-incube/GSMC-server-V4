b56f457844c43b7f35d09e7ca9f2b0843633e570
jyx-07 <s25069@gsm.hs.kr>
2026-07-10 00:37:40 +0900

    feat(score): 점수 추가/승인/거절/삭제 GraphQL 스키마 및 리졸버 연결

--- src/main/kotlin/team/incube/gsmc/domain/score/adapter/web/ScoreWebAdapter.kt
+++ src/main/kotlin/team/incube/gsmc/domain/score/adapter/web/ScoreWebAdapter.kt
@@ -1,6 +1,7 @@
 package team.incube.gsmc.domain.score.adapter.web
 
 import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
 import team.incube.gsmc.domain.category.CategoryType
@@ -9,6 +10,11 @@ import team.incube.gsmc.domain.score.Score
 import team.incube.gsmc.domain.score.ScoreCategoryGroup
 import team.incube.gsmc.domain.score.ScoreStatus
 import team.incube.gsmc.domain.score.TotalScore
+import team.incube.gsmc.domain.score.port.`in`.AppendMyScoreOnlyUseCase
+import team.incube.gsmc.domain.score.port.`in`.AppendMyScoreWithEvidenceUseCase
+import team.incube.gsmc.domain.score.port.`in`.AppendMyScoreWithFileUseCase
+import team.incube.gsmc.domain.score.port.`in`.AppendMyScoreWithValueUseCase
+import team.incube.gsmc.domain.score.port.`in`.ApproveScoreUseCase
 import team.incube.gsmc.domain.score.port.`in`.FetchMyPercentInClassUseCase
 import team.incube.gsmc.domain.score.port.`in`.FetchMyPercentInGradeUseCase
 import team.incube.gsmc.domain.score.port.`in`.FetchMyScoresByCategoryUseCase
@@ -17,10 +23,12 @@ import team.incube.gsmc.domain.score.port.`in`.FetchMyTotalScoreUseCase
 import team.incube.gsmc.domain.score.port.`in`.FetchScoreUseCase
 import team.incube.gsmc.domain.score.port.`in`.FetchScoresByCategoryUseCase
 import team.incube.gsmc.domain.score.port.`in`.FetchTotalScoreUseCase
+import team.incube.gsmc.domain.score.port.`in`.RejectScoreUseCase
+import team.incube.gsmc.domain.score.port.`in`.RemoveScoreUseCase
 
 /**
- * 점수 조회 GraphQL Query 리졸버입니다.
- * 각 Query를 대응하는 UseCase에 위임하는 것 외의 비즈니스 로직은 갖지 않습니다.
+ * 점수 조회/추가/승인/거절/삭제 GraphQL Query·Mutation 리졸버입니다.
+ * 각 Query/Mutation을 대응하는 UseCase에 위임하는 것 외의 비즈니스 로직은 갖지 않습니다.
  */
 @Controller
 class ScoreWebAdapter(
@@ -32,6 +40,13 @@ class ScoreWebAdapter(
     private val fetchTotalScoreUseCase: FetchTotalScoreUseCase,
     private val fetchMyPercentInClassUseCase: FetchMyPercentInClassUseCase,
     private val fetchMyPercentInGradeUseCase: FetchMyPercentInGradeUseCase,
+    private val appendMyScoreWithFileUseCase: AppendMyScoreWithFileUseCase,
+    private val appendMyScoreWithEvidenceUseCase: AppendMyScoreWithEvidenceUseCase,
+    private val appendMyScoreWithValueUseCase: AppendMyScoreWithValueUseCase,
+    private val appendMyScoreOnlyUseCase: AppendMyScoreOnlyUseCase,
+    private val approveScoreUseCase: ApproveScoreUseCase,
+    private val rejectScoreUseCase: RejectScoreUseCase,
+    private val removeScoreUseCase: RemoveScoreUseCase,
 ) {
     @QueryMapping
     fun myScores(
@@ -75,4 +90,43 @@ class ScoreWebAdapter(
     fun myPercentInGrade(
         @Argument includeApprovedOnly: Boolean?,
     ): Percentile = fetchMyPercentInGradeUseCase.execute(includeApprovedOnly ?: true)
+
+    @MutationMapping
+    fun addScoreWithFile(
+        @Argument categoryType: CategoryType,
+        @Argument input: ScoreWithValueAndFileInput,
+    ): Score = appendMyScoreWithFileUseCase.execute(categoryType, input.value, input.fileId)
+
+    @MutationMapping
+    fun addScoreWithEvidence(
+        @Argument categoryType: CategoryType,
+        @Argument input: ScoreWithValueInput,
+    ): Score = appendMyScoreWithEvidenceUseCase.execute(categoryType, input.value)
+
+    @MutationMapping
+    fun addScoreWithValue(
+        @Argument categoryType: CategoryType,
+        @Argument input: ScoreWithValueInput,
+    ): Score = appendMyScoreWithValueUseCase.execute(categoryType, input.value)
+
+    @MutationMapping
+    fun addScoreOnly(
+        @Argument categoryType: CategoryType,
+    ): Score = appendMyScoreOnlyUseCase.execute(categoryType)
+
+    @MutationMapping
+    fun approveScore(
+        @Argument scoreId: Long,
+    ): Boolean = approveScoreUseCase.execute(scoreId)
+
+    @MutationMapping
+    fun rejectScore(
+        @Argument scoreId: Long,
+        @Argument input: RejectScoreInput,
+    ): Boolean = rejectScoreUseCase.execute(scoreId, input.rejectionReason)
+
+    @MutationMapping
+    fun deleteScore(
+        @Argument scoreId: Long,
+    ): Boolean = removeScoreUseCase.execute(scoreId)
 }
