7a37c8008c4a8537a8104f72039d0090f46490f6
jyx-07 <s25069@gsm.hs.kr>
2026-07-14 11:17:30 +0900

    test(category): 카테고리 조회/검색 서비스 테스트 추가

---
+++ src/test/kotlin/team/incube/gsmc/domain/category/service/SearchCategoriesServiceTest.kt
@@ -0,0 +1,69 @@
+package team.incube.gsmc.domain.category.service
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.clearAllMocks
+import io.mockk.every
+import io.mockk.mockk
+import team.incube.gsmc.domain.category.Category
+import team.incube.gsmc.domain.category.CategoryType
+import team.incube.gsmc.domain.category.EvidenceType
+import team.incube.gsmc.domain.category.ScoreCalculationType
+import team.incube.gsmc.domain.category.port.out.CategoryPersistencePort
+
+class SearchCategoriesServiceTest :
+    BehaviorSpec({
+        val categoryPersistencePort = mockk<CategoryPersistencePort>()
+        val service = SearchCategoriesService(categoryPersistencePort)
+
+        beforeEach { clearAllMocks() }
+
+        val category =
+            Category(
+                categoryId = 1L,
+                weight = 1,
+                categoryEnglishName = "Certificate",
+                categoryKoreanName = "자격증",
+                categoryMaximumValue = 5,
+                isAccumulated = false,
+                evidenceType = EvidenceType.FILE,
+                categoryType = CategoryType.CERTIFICATE,
+                calculationType = ScoreCalculationType.COUNT_BASED,
+            )
+
+        Given("keyword가 주어졌을 때") {
+            When("execute를 호출하면") {
+                Then("영속성 포트의 searchByKeyword 결과를 반환한다") {
+                    every { categoryPersistencePort.searchByKeyword("자격증") } returns listOf(category)
+
+                    val result = service.execute("자격증")
+
+                    result shouldBe listOf(category)
+                }
+            }
+        }
+
+        Given("keyword가 null일 때") {
+            When("execute를 호출하면") {
+                Then("전체 카테고리를 반환한다") {
+                    every { categoryPersistencePort.findAll() } returns listOf(category)
+
+                    val result = service.execute(null)
+
+                    result shouldBe listOf(category)
+                }
+            }
+        }
+
+        Given("keyword가 빈 문자열/공백일 때") {
+            When("execute를 호출하면") {
+                Then("전체 카테고리를 반환한다") {
+                    every { categoryPersistencePort.findAll() } returns listOf(category)
+
+                    val result = service.execute("   ")
+
+                    result shouldBe listOf(category)
+                }
+            }
+        }
+    })
