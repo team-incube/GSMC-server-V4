d005ea2e7d481e8a13e0d1d1c6a64503e35a5451
snowykte0426 <snowykte0426@naver.com>
2026-08-14 21:36:47 +0900

    refactor(ci/cd): 의존성 및 빌드 플러그인 최신 버전으로 업그레이드

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,7 +1,7 @@
 object Libs {
     // SDK
     const val THE_SDK = "com.github.themoment-team:the-sdk:1.5"
-    const val DATAGSM_OAUTH_SDK = "com.github.themoment-team:datagsm-oauth-sdk-java:1.5.0"
+    const val DATAGSM_OAUTH_SDK = "com.github.themoment-team:datagsm-oauth-sdk-java:1.6.0"
 
     // Spring Boot
     const val SPRING_BOOT_JPA = "org.springframework.boot:spring-boot-starter-data-jpa"
@@ -20,19 +20,19 @@ object Libs {
     const val FLYWAY_MYSQL = "org.flywaydb:flyway-mysql"
 
     // QueryDSL
-    const val QUERYDSL_JPA = "io.github.openfeign.querydsl:querydsl-jpa:6.10.1"
-    const val QUERYDSL_KAPT = "io.github.openfeign.querydsl:querydsl-apt:6.10.1:jakarta"
+    const val QUERYDSL_JPA = "io.github.openfeign.querydsl:querydsl-jpa:7.5"
+    const val QUERYDSL_KAPT = "io.github.openfeign.querydsl:querydsl-apt:7.5:jakarta"
 
     // JWT
-    const val JJWT_API = "io.jsonwebtoken:jjwt-api:0.12.6"
-    const val JJWT_IMPL = "io.jsonwebtoken:jjwt-impl:0.12.6"
-    const val JJWT_JACKSON = "io.jsonwebtoken:jjwt-jackson:0.12.6"
+    const val JJWT_API = "io.jsonwebtoken:jjwt-api:0.13.0"
+    const val JJWT_IMPL = "io.jsonwebtoken:jjwt-impl:0.13.0"
+    const val JJWT_JACKSON = "io.jsonwebtoken:jjwt-jackson:0.13.0"
 
     // Swagger
-    const val SPRINGDOC_OPENAPI = "org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8"
+    const val SPRINGDOC_OPENAPI = "org.springdoc:springdoc-openapi-starter-webmvc-ui:3.1.0"
 
     // GraphQL Extended Scalars
-    const val GRAPHQL_EXTENDED_SCALARS = "com.graphql-java:graphql-java-extended-scalars:22.0"
+    const val GRAPHQL_EXTENDED_SCALARS = "com.graphql-java:graphql-java-extended-scalars:24.0"
 
     // Util
     const val LOMBOK = "org.projectlombok:lombok"
@@ -42,8 +42,8 @@ object Libs {
     const val KOTLIN_TEST_JUNIT5 = "org.jetbrains.kotlin:kotlin-test-junit5"
     const val SPRING_GRAPHQL_TEST = "org.springframework.graphql:spring-graphql-test"
     const val SPRING_SECURITY_TEST = "org.springframework.security:spring-security-test"
-    const val MOCKK = "io.mockk:mockk:1.14.9"
-    const val KOTEST_RUNNER = "io.kotest:kotest-runner-junit5:5.9.1"
-    const val KOTEST_ASSERTIONS = "io.kotest:kotest-assertions-core:5.9.1"
+    const val MOCKK = "io.mockk:mockk:1.14.11"
+    const val KOTEST_RUNNER = "io.kotest:kotest-runner-junit5:6.2.4"
+    const val KOTEST_ASSERTIONS = "io.kotest:kotest-assertions-core:6.2.4"
     const val JUNIT_PLATFORM_LAUNCHER = "org.junit.platform:junit-platform-launcher"
 }
